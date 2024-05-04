bd7ef25c75c433597ed41e857a591f0fda26062d
Daniel Quintero <147358058+Dak30@users.noreply.github.com>
2024-05-04 05:12:12 +0000

    version inicial

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sqlite3
-
 from flask import Flask, render_template
-
+import sqlite3
 from pprint import pprint
+
+from flask.helpers import redirect
 
 conexion = sqlite3.connect('web2.sqlite3')
 conexion.row_factory = sqlite3.Row
@@ -11,7 +11,6 @@ cursor.execute("""
 SELECT * FROM productos;
 """)
 productos = [dict(producto) for producto in cursor.fetchall()]
-pprint(productos)
 cursor.close()
 conexion.close()
 
@@ -23,3 +22,15 @@ app = Flask(__name__)
 @app.route('/')
 def ruta_raiz():
   return render_template('index.html', productos=productos)
+
+@app.route('/producto/<int:pid>')
+def ruta_producto(pid):
+  for producto in productos:
+    if pid == producto['id']:
+      return render_template('producto.html', producto=producto')
+  return redirect('/')
+
+#progrma principal
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
